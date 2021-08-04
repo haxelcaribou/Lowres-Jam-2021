33427be80afcbb5d3a4da1d15597659b1b7989f9
Void <joaolucas697@gmail.com>
2021-08-03 22:33:08 -0300

    Working on Movent

--- character.gd
+++ character.gd
@@ -1,13 +1,17 @@
-extends Sprite
+extends KinematicBody2D
 
-var vel : Vector2
-
+var vel = Vector2()
+var speed = 30
 func movement_input():
 	if Input.is_action_pressed("move_up"):
-		vel.y -= 1
+		vel.y -= 0.5
 	if Input.is_action_pressed("move_down"):
-		vel.y += 1
+		vel.y += 0.5
 	if Input.is_action_pressed("move_left"):
-		vel.y -= 1
+		vel.y -= 0.5
 	if Input.is_action_pressed("move_right"):
-		vel.y -= 1
+		vel.y -= 0.5
+	vel = vel.normalized() * speed
+
+func _physics_process(delta):
+	vel = move_and_slide(vel)
